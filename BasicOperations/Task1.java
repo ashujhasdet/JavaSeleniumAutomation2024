867b6914c9eb7c8143826c455f9083915a14bcd9
Aashutosh Jha <145312210+ashujhasdet@users.noreply.github.com>
2024-03-07 19:53:39 +0530

    Add files via upload

---
+++ BasicOperations/Task1.java
@@ -0,0 +1,22 @@
+package automationProgramsset1;
+//code to launch the browser from eclipse
+
+import org.openqa.selenium.chrome.ChromeDriver;//To launch Chrome Browser
+import org.openqa.selenium.firefox.FirefoxDriver;//To Launch FireFox Browser
+
+public class Task1 {
+	public static void main(String [] args) {
+		//code to launch Chrome Browser
+		//create an object of ChromeDriver class
+		ChromeDriver driver1 = new ChromeDriver();//make sure to import required Chrome-driver
+		//Similarly we can Launch Other browsers from here 
+		/*
+		 Code For FireFox Browser
+		 FirefoxDriver driver2 = new FirefoxDriver();
+		 Remember to import the Firefox driver
+		 
+		 */
+		
+	}
+
+}
